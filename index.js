784b12c15f28a65e914f4370bef0c1b5f231160e
Rubel <hossainrobiul33@gmail.com>
2024-02-08 21:47:05 +0600

    final commit

--- index.js
+++ index.js
@@ -1,44 +1,40 @@
-function handleOnClick(){
-    let username = document.getElementById("username").value;
-    let password = document.getElementById("password").value;
-    alert(`Hello,${username}`);
-}
-document.getElementById("form").addEventListener("submit",function(event){
+let ui1 = document.getElementById("pv");
+let ui2 = document.getElementById("uv");
+let form = document.getElementById("form");
+
+form.addEventListener("submit",function(event){
     event.preventDefault();
     let username = document.getElementById("username").value;
     let password = document.getElementById("password").value;
-    if(validate(password,username)){
+    if(isValid(username,password)){
         alert(`Hello,${username}`);
-    }else{
-        //document.getElementById("pv").style.visibility = "visible";
     }
+    setTimeout(() => {
+        ui1.style.visibility = "hidden";
+        ui2.style.visibility = "hidden";
+    }, 1000);
 });
-function validate(password,username){
-    let ui = document.getElementById("pv");
-    if(password.length < 8){
-        ui.style.visibility = "visible";
-        ui.innerText = "Password length less than 8";
+
+
+function isValid(username,password){
+    if (username.trim() === '' && password.trim() === '') {
+        console.log("v");
+        ui1.style.visibility = "visible";
+        ui1.innerText = "Please Enter Password.";
+        ui2.style.visibility = "visible"
+        ui2.innerText = "Please Enter Username.";
         return false;
-    }
-    if(!/[a-zA-Z]/.test(password)){
-        ui.style.visibility = "visible";
-        ui.innerText = "Password doesn't have character";
+    }else if(password.trim() === '' && username.trim() !== ''){
+        console.log("p");
+        ui1.style.visibility = "visible";
+        ui1.innerText = "Please Enter Password.";
         return false;
-    }
-    if(!/\d/.test(password)){
-        ui.style.visibility = "visible";
-        ui.innerText = "Password doesn't have number";
+    }else if(username.trim() === '' && password.trim() !== ''){
+        console.log("u");
+        ui2.style.visibility = "visible";
+        ui2.innerText = "Please Enter Username.";
         return false;
+    }else{
+        return true;
     }
-    if(!/[!@#$%^&*()_+\-=[\]{};':"\\|,.<>/?]/.test(password)){
-        ui.style.visibility = "visible";
-        ui.innerText = "Password doesn't have special character";
-        return false;
-    }
-    if(username.length ==0){
-        document.getElementById("uv").style.visibility = "visible";
-        return false;
-    }
-    ui.style.visibility = "hidden";
-    return true;
 }
